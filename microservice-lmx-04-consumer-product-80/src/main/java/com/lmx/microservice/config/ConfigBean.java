cb401a4758e33b2e488dff5beb5f525b81c34e7e
limingxing <451721318@qq.com>
2019-12-15 15:06:29 +0800

    添加zuul

--- microservice-lmx-04-consumer-product-80/src/main/java/com/lmx/microservice/config/ConfigBean.java
+++ microservice-lmx-04-consumer-product-80/src/main/java/com/lmx/microservice/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.lmx.microservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration //标识配置类
 public class ConfigBean {
 
-
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
--- microservice-lmx-04-consumer-product-80/src/main/java/com/lmx/microservice/controller/ProductController_Consumer.java
+++ microservice-lmx-04-consumer-product-80/src/main/java/com/lmx/microservice/controller/ProductController_Consumer.java
@@ -12,7 +12,12 @@ import java.util.List;
 @RestController
 public class ProductController_Consumer {
 
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    //private static final String REST_URL_PREFIX = "http://localhost:8001";
+    /**
+     * 服务名就相当于IP+端口号
+     * microservice-product = localhost:8001 = 127.0.0.1:8001
+     */
+    private static final String REST_URL_PREFIX = "http://microservice-product";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -31,6 +36,5 @@ public class ProductController_Consumer {
     public List<Product> list() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/product/list", List.class);
     }
-
 
 }
